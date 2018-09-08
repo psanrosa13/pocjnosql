7419dbbad6a633af2b1954ce27075848e438f662
psanrosa13 <psanrosa13@gmmail.com>
2018-09-08 19:34:58 -0300

    segundo commit- alterações para CDI

--- src/main/java/pocnosql/pocnosql/App.java
+++ src/main/java/pocnosql/pocnosql/App.java
@@ -4,10 +4,7 @@ import javax.enterprise.inject.Instance;
 import javax.enterprise.inject.se.SeContainer;
 import javax.enterprise.inject.se.SeContainerInitializer;
 
-import org.jnosql.artemis.Database;
 import org.jnosql.artemis.DatabaseQualifier;
-import org.jnosql.artemis.Databases;
-import org.jnosql.artemis.key.KeyValueTemplate;
 
 /**
  * Hello world!
@@ -17,17 +14,17 @@ public class App {
 	public static void main(String[] args) {
 		try (SeContainer container = SeContainerInitializer.newInstance().initialize()) {
 
-//			Instance<UsuarioRepository> instance = container.select(UsuarioRepository.class,
-//					DatabaseQualifier.ofKeyValue());
+			Instance<UsuarioRepository> instance = container.select(UsuarioRepository.class,
+					DatabaseQualifier.ofKeyValue());
 
-//			UsuarioRepository usuarioRepository = instance.get();
+			UsuarioRepository usuarioRepository = instance.get();
 
-			KeyValueTemplate template = container.select(KeyValueTemplate.class).get();
+//			KeyValueTemplate template = container.select(KeyValueTemplate.class).get();
 			Usuario usuario = new Usuario();
-			usuario.setEmail("paula234@teste.com");
-			usuario.setNome("Paula");
-			template.put(usuario);
-//			usuarioRepository.save(usuario);
+			usuario.setEmail("teste@teste.com");
+			usuario.setNome("teste");
+//			template.put(usuario);
+			usuarioRepository.save(usuario);
 
 		}
 	}
